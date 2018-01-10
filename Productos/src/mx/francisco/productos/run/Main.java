369dd44bf6d3ca577559edb90a7a0d1d32b731e9
fgb <fgb@localhost.localdomain>
2018-01-09 18:59:38 -0600

    se agrego la clase principal del programa

---
+++ Productos/src/mx/francisco/productos/run/Main.java
@@ -0,0 +1,13 @@
+
+package mx.francisco.productos.run;
+
+/**
+ *
+ * @author fgb
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
