a71ad4c1a320fbbe3c0c9e7810892b9d49cdec43
Francisco González <34868534+FranciscoGB03@users.noreply.github.com>
2018-01-09 19:01:33 -0600

    Update Main.java

--- Productos/src/mx/francisco/productos/run/Main.java
+++ Productos/src/mx/francisco/productos/run/Main.java
@@ -8,6 +8,7 @@ package mx.francisco.productos.run;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("prueba 2");
     }
     
 }
